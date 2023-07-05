68abe5e4662284eba7625ffa7936407ab86f7539
abdulbasit-dev <abdullbasit99salah@gmail.com>
2023-07-05 12:48:13 +0300

    change data

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Tilt from "react-tilt";
-import { motion } from "framer-motion";
+import React from 'react';
+import Tilt from 'react-tilt';
+import {motion} from 'framer-motion';
 
-import { styles } from "../styles";
-import { services } from "../constants";
-import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion";
+import {styles} from '../styles';
+import {services} from '../constants';
+import {SectionWrapper} from '../hoc';
+import {fadeIn, textVariant} from '../utils/motion';
 
-const ServiceCard = ({ index, title, icon }) => (
+const ServiceCard = ({index, title, icon}) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
@@ -21,15 +21,9 @@ const ServiceCard = ({ index, title, icon }) => (
         }}
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
+        <img src={icon} alt='web-development' className='w-16 h-16 object-contain' />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
+        <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
       </div>
     </motion.div>
   </Tilt>
@@ -43,18 +37,13 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+      <motion.p variants={fadeIn('', '', 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        I'm a dedicated full-stack Laravel developer specializing in backend development. With extensive experience in Laravel, I bring
+        deep expertise in building robust, scalable, and efficient web applications. I prioritize delivering high-quality code and ensuring
+        seamless functionality. Let's collaborate to bring your Laravel projects to new heights!
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex justify-center flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
@@ -63,4 +52,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, "about");
+export default SectionWrapper(About, 'about');
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915eff]'>Basit</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Transforming ideas into reality, with <br className='sm:block hidden' />
+            Laravel-powered web development.
           </p>
         </div>
       </div>
